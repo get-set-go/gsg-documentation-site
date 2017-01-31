8e83f4477b60e8e08f3419fa7297a28219cd4f05
hegdeashwin <ashwin.hegde3@gmail.com>
2017-02-01 01:40:31 +0530

    Update documentation site

--- src/components/CodeBlocks/CodeBlocks.jsx
+++ src/components/CodeBlocks/CodeBlocks.jsx
@@ -6,8 +6,12 @@ export default class CodeBlocks extends React.Component {
   render() {
     return (
       <div className="component-codeblocks">
-        <h3>Code Examples</h3>
+        <h4>{this.props.title}</h4>
         
+        {this.props.children}
+        <pre>
+          {this.props.code}
+        </pre>
       </div>
     );
   }
