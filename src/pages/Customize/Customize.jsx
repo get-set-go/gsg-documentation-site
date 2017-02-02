285a667aa4706cd6dced9a156651dcc7692e189d
hegdeashwin <ashwin.hegde3@gmail.com>
2017-02-02 23:44:28 +0530

    Add Customize component

---
+++ src/pages/Customize/Customize.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Customize extends React.Component {
+  render() {
+    return (
+      <div className="row">
+        Coming Soon
+      </div>
+    );
+  }
+}
