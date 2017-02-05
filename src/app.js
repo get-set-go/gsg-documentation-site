ff049e989117f9aea1fbfa65547adf67672b8f99
hegdeashwin <ashwin.hegde3@gmail.com>
2017-02-05 23:40:38 +0530

    Integrate Footer + update home

--- src/app.js
+++ src/app.js
@@ -6,15 +6,15 @@ import '../node_modules/font-awesome/css/font-awesome.min.css';
 import React from 'react';
 
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="container">
         <Header />
-        <div>
-          {this.props.children}
-        </div>
+        {this.props.children}
+        <Footer />
       </div>
     );
   }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,21 +12,16 @@ export default class Home extends React.Component {
         <div className="jumbotron">
           <div className="container">
             <div className="row">
-              <div className="col-md-4 text-center">
+              <div className="col-md-12 text-center">
                 <img src={logo} />
-              </div>
-              <div className="col-md-8">
-                <h1>Get-Set-Go <small><sup>v0.0.1</sup></small></h1>
+
+                <h1>Get-Set-Go</h1>
                 <p className="lead">A React, Bootstrap based components for React projects on the web.</p>
 
                 <a className="github-button" href="https://github.com/get-set-go/gsg-documentation-site/subscription" data-icon="octicon-eye" data-style="mega" data-count-href="/get-set-go/gsg-documentation-site/watchers" data-count-api="/repos/get-set-go/gsg-documentation-site#subscribers_count" data-count-aria-label="# watchers on GitHub" aria-label="Watch get-set-go/gsg-documentation-site on GitHub">Watch</a>
                 <a className="github-button" href="https://github.com/get-set-go/gsg-documentation-site" data-icon="octicon-star" data-style="mega" data-count-href="/get-set-go/gsg-documentation-site/stargazers" data-count-api="/repos/get-set-go/gsg-documentation-site#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star get-set-go/gsg-documentation-site on GitHub">Star</a>
                 <a className="github-button" href="https://github.com/get-set-go/gsg-documentation-site/fork" data-icon="octicon-repo-forked" data-style="mega" data-count-href="/get-set-go/gsg-documentation-site/network" data-count-api="/repos/get-set-go/gsg-documentation-site#forks_count" data-count-aria-label="# forks on GitHub" aria-label="Fork get-set-go/gsg-documentation-site on GitHub">Fork</a>
                 <a className="github-button" href="https://github.com/get-set-go" data-style="mega" data-count-href="/get-set-go/followers" data-count-api="/users/get-set-go#followers" data-count-aria-label="# followers on GitHub" aria-label="Follow @get-set-go on GitHub">Follow @get-set-go</a>
-
-                <p className="btn-collection">
-                  <Link to="components" className="btn btn-primary">See Components</Link>
-                </p>
 
               </div>
             </div>
@@ -35,7 +30,7 @@ export default class Home extends React.Component {
 
         <div className="text-center">
           <p className="lead">Get-Set-Go is an open source project. Its hosted, developed, and maintained on GitHub.</p>
-          <p><a className="btn btn-success" href="//github.com/get-set-go" target="_blank">View Get-Set-Go projects on Github</a></p>
+          <p><a className="btn btn-lg btn-success" href="//github.com/get-set-go" target="_blank">View Get-Set-Go projects on Github</a></p>
         </div>
 
       </div>
